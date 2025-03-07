f07b8df41defa884a6139eb384e974b57d1ffd60
ronei-alves <ronei.alves@zoho.com>
2025-03-07 12:45:55 -0300

    TODO

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        //TODO: Conhecer e importar a classe Scanner
 
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela Scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
     }
 }
